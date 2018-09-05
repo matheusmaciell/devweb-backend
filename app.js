f4c490124f71726e875481fa85d140b37115dde1
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2018-09-05 17:08:35 -0300

    documentacao e morgan

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const app = express()
+const app = express();
 const router = express.Router();
+var morgan = require('morgan');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
 
 //app.get('/user', function(req, res) {
 //  res.status(200).json({ name: 'john' });
@@ -12,6 +13,8 @@ const home = require('./home');
 const user = require('./user/userRoute');
 const classe = require('./class/classRoute');
 const school = require('./school/schoolRoute');
+
+app.use(morgan('tiny'));
 app.use('/',home);
 app.use('/user', user);
 app.use('/class',classe);
@@ -20,6 +23,5 @@ app.use('/school',school);
 //essa parte serve pra pegar a imagem que ta no diretorio static, e tem como endpoint '/imagem'
 app.use('/imagem',express.static(__dirname+'/static'))
 
-
-
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports = app;
